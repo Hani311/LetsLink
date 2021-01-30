894c13991c396ecb16bd773952eceee6e2812676
OussamaMhSafi <oussamamhsafi@gmail.com>
2021-01-30 19:57:30 +0100

    second commit

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+//import com.example.androidproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //DataBindingUtil binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflator, R.layout.fragment_title, container, false)
+        //ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         setContentView(R.layout.activity_main);
     }
 }
