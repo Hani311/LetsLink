5a1de56ab9c76c15e47b00880d300997391e2270
OussamaMhSafi <oussamamhsafi@gmail.com>
2021-02-02 15:33:50 +0100

    did stuff

--- app/src/main/java/com/example/androidproject/LoginFragment.kt
+++ app/src/main/java/com/example/androidproject/LoginFragment.kt
@@ -1,19 +1,17 @@
 package com.example.androidproject
 
+import android.app.Person
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.example.androidproject.databinding.FragmentTitleBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.example.androidproject.databinding.FragmentLoginBinding
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,12 +21,36 @@ class LoginFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_login, container, false)
 
-        val binding: FragmentTitleBinding= DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        val binding: FragmentLoginBinding= DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        val usernameText=binding.usernameInput
+        val userPassword=binding.passwordInput
+        val signUpButton=binding.signUpButton
+        val loginButton=binding.logInButton
+
+
+        signUpButton.setOnClickListener(){
+            signUp()
+        }
+
+        loginButton.setOnClickListener(){
+            logIn(usernameText, userPassword)
+        }
+
         return binding.root
+    }
+
+    private fun signUp() {
+
+    }
+
+    private fun logIn(usernameText: TextInputLayout, userPassword: TextInputLayout) {
+
+
     }
 }
