d1717abfcdc90a9569c392f85465b0cb29bc4677
StivHKR <45165596+StivHKR@users.noreply.github.com>
2021-02-11 21:43:15 +0100

    profile pic

---
+++ app/src/main/java/com/example/androidproject/ProfileNavigationFragment.java
@@ -0,0 +1,30 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileNavigationFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_navigation_fragment, container, false);
+    }
+}
