f728947c774061b372c41f6d876d0eff2787564b
AliHabesh <57945214+AliHabesh@users.noreply.github.com>
2021-03-08 20:04:58 +0100

    Ali (#18)
    * added profile settings with gallery option
    
    * Just added parts of the map, need to add others and fix some stuff
    
    * added each user to event, i will need to fix the currentLocation problem im having and last thing the Corona limits.

---
+++ app/src/main/java/com/example/androidproject/Events.java
@@ -0,0 +1,83 @@
+package com.example.androidproject;
+
+public class Events {
+
+    private String eventType;
+    private double longitude;
+    private double latitude;
+    private String description;
+    private String capacity;
+    private String eventName;
+
+    public Events() {
+    }
+
+    public Events(String eventType, String eventName, double longitude, double latitude, String description, String capacity) {
+        this.eventType = eventType;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.eventName = eventName;
+        this.description = description;
+        this.capacity = capacity;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Events{" +
+                "eventType='" + eventType + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", description='" + description + '\'' +
+                ", capacity='" + capacity + '\'' +
+                '}';
+    }
+}
+
