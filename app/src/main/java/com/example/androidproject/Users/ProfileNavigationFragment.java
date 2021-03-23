bfdf77d4abee1b7af5fb8ea1fc667d9640e363aa
StivHKR <45165596+StivHKR@users.noreply.github.com>
2021-03-23 14:13:33 +0100

    Bug with Findfriend Fixed / added Reset pass

--- app/src/main/java/com/example/androidproject/Users/ProfileNavigationFragment.java
+++ app/src/main/java/com/example/androidproject/Users/ProfileNavigationFragment.java
@@ -76,7 +76,7 @@ public class ProfileNavigationFragment extends Fragment {
         Button  skip=view.findViewById(R.id.profileBtn);
          Button     btnFriendsList=view.findViewById(R.id.btnFriends);
          Button btnSearch= view.findViewById(R.id.searchFrinds);
-         Button btnthre= view.findViewById(R.id.button3);
+
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,13 +98,7 @@ public class ProfileNavigationFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        btnthre.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (getActivity(), ResetPasswordActivity.class);
-                startActivity(intent);
-            }
-        });
+
         // Inflate the layout for this fragment
         return view;
     }
--- app/src/main/java/com/example/androidproject/Users/ResetPasswordActivity.java
+++ app/src/main/java/com/example/androidproject/Users/ResetPasswordActivity.java
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.androidproject.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,21 +19,19 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class ResetPasswordActivity extends AppCompatActivity {
     private FirebaseUser user;
-    private  TextInputEditText oldPass,newPass;
-    private Button btnReset;
-    private View coordinatorLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-        btnReset = findViewById(R.id.btnReset);
-        oldPass= findViewById(R.id.textInputOldPassword);
-        String oldpass= oldPass.getText().toString();
-        newPass= findViewById(R.id.textInputNewPassword);
-        String newpass= oldPass.getText().toString();
-        btnReset.setOnClickListener(new View.OnClickListener() {
+        Button btnChange= findViewById(R.id.btnChange);
+        btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText oldPass= findViewById(R.id.textInputOldPassword);
+                String oldpass= oldPass.getText().toString();
+                EditText  newPass= findViewById(R.id.textInputNewPassword);
+                String newpass= newPass.getText().toString();
                 user = FirebaseAuth.getInstance().getCurrentUser();
                 final String email = user.getEmail();
                 AuthCredential credential = EmailAuthProvider.getCredential(email,oldpass);
@@ -46,28 +44,18 @@ public class ResetPasswordActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if(!task.isSuccessful()){
-                                        Snackbar snackbar_fail = Snackbar
-                                                .make(coordinatorLayout, "Something went wrong. Please try again later", Snackbar.LENGTH_LONG);
-                                        snackbar_fail.show();
+                                        Toast.makeText(ResetPasswordActivity.this, "Something went wrong. Please try again later", Toast.LENGTH_SHORT).show();
                                     }else {
-                                        Snackbar snackbar_su = Snackbar
-                                                .make(coordinatorLayout, "Password Successfully Modified", Snackbar.LENGTH_LONG);
-                                        snackbar_su.show();
+                                        Toast.makeText(ResetPasswordActivity.this, "Password Successfully Modified", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
                         }else {
-                            Snackbar snackbar_su = Snackbar
-                                    .make(coordinatorLayout, "Authentication Failed", Snackbar.LENGTH_LONG);
-                            snackbar_su.show();
+                            Toast.makeText(ResetPasswordActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
-
-
     }
-
 }
-
