a8c4cca3482deb3485ec5969631ea118e23aeb96
Hani311 <hani_adel.alzir0072@stud.hkr.se>
2021-03-25 05:52:45 +0100

    more layout changes

--- app/src/main/java/com/example/androidproject/Map/MapsFragment.java
+++ app/src/main/java/com/example/androidproject/Map/MapsFragment.java
@@ -1,11 +1,6 @@
 package com.example.androidproject.Map;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -31,7 +26,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.androidproject.Chat.ChatFragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
 import com.example.androidproject.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -503,9 +502,9 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                             System.out.println(lats + "     " + lon);
                             int event = 0;
                             if (type.equals("sport")) {
-                                event = R.drawable.sport;
+                                event = R.drawable.sports;
                             } else if (type.equals("party")) { // add more icons later
-                                event = R.drawable.party;
+                                event = R.drawable.partys;
                             }
 
                             gMap = googleMap;
