90925b7805c90f7cbfffd34f5416eeaf07a75179
AliHabesh <ali.habesh0068@stud.hkr.se>
2021-03-25 21:03:51 +0100

    Added a task Scheduler thread that executes at a given time

---
+++ app/src/main/java/com/example/androidproject/Map/EventScheduler.java
@@ -0,0 +1,38 @@
+package com.example.androidproject.Map;
+
+import android.app.Dialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.androidproject.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.TimerTask;
+
+import static com.facebook.appevents.AppEventsLogger.getUserID;
+
+public class EventScheduler extends TimerTask {
+
+    //private GoogleMap gMap;
+
+
+    @Override
+    public void run() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Events");
+        DatabaseReference MemberCountRef = FirebaseDatabase.getInstance().getReference("Joined Member");
+        DatabaseReference joinedMemberRef = FirebaseDatabase.getInstance().getReference("Joined Users");
+        DatabaseReference groupChatRef = FirebaseDatabase.getInstance().getReference("Groups");
+                    reference.removeValue();
+                    MemberCountRef.removeValue();
+                    joinedMemberRef.removeValue();
+                    groupChatRef.removeValue();
+                 //   gMap.clear();
+                  // MapsFragment m = new MapsFragment();
+                 //  m.spawnNearbyEventsOnMap(gMap);
+
+
+    }
+}
